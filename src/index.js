9d0da8c1f08a8b43c3ccfaf8b1067dd3301dd78c
Reedy Feggins <rfeggins@us.ibm.com>
2019-11-25 10:15:18 -0500

    md-1233 add messaging to index.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import uuidv4 from 'uuid/v4';
 
 const app = express();
 
@@ -40,12 +41,41 @@ let users = {
   },
 };
 
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
 app.get('/users', (req, res) => {
     return res.send(Object.values(users));
   });
 
 app.get('/users/:userId', (req, res) => {
     return res.send(users[req.params.userId]);
+  });
+
+  app.get('/messages', (req, res) => {
+    return res.send(Object.values(messages));
+  });
+  app.get('/messages/:messageId', (req, res) => {
+    return res.send(messages[req.params.messageId]);
+  });
+
+  app.post('/messages', (req, res) => {
+    const id = uuidv4();
+    const message = {
+      id,
+    }
+    message[id] = message;
+    return res.send(message);
   });
 
 // Add PORT=<port no> to .env file in project directory
